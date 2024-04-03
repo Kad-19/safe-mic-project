ee3062212916f136b9415cd60e8c827fd9978de7
Natidev <53361365+Natidev@users.noreply.github.com>
2024-04-03 16:18:58 +0300

    shadecn setup

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,18 @@ import React from "react";
 import LoginPage from "./component/LoginPage/LoginPage";
 import Navbar from "./component/Navbar/Navbar";
 import Complaint from './component/Complaint/Complaint'
+import { Button } from "./components/ui/button";
+import { Calendar } from "./components/ui/calendar";
+import { cn } from "./lib/utils";
 const App = () => {
-  return (
-  <Complaint/>
+  return (<div>
+      <Complaint/>
+      <div className="flex justify-center">
+        <Calendar className={cn('')}/>
+      </div>
+
+  </div>
+
   )
 }
 export default App;
