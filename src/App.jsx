e32c97f2934f12f44ed9106ce632c98e8641df71
Natidev <53361365+Natidev@users.noreply.github.com>
2024-04-03 17:53:45 +0300

    shadecn
    shadecn is installed

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,12 @@
 import React from "react";
-import LoginPage from "./component/LoginPage/LoginPage"
-import Reg from "./component/LoginPage/Reg";
 import Navbar from "./component/Navbar/Navbar";
-import Complaint from './component/Complaint/Complaint'
 import AllRouting from "./component/AllRouting/AllRouting";
 const App = () => {
   return (
   <div> 
     <Navbar/>
     <AllRouting/>
-     
   </div>
-  
-
   )
 }
 export default App;
