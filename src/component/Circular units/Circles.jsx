874c34551d1b35bedb75bdd4581788f8d7ec00fa
Natidev <53361365+Natidev@users.noreply.github.com>
2024-04-05 16:44:10 +0300

    framer motion is installed

--- src/component/Circular units/Circles.jsx
+++ src/component/Circular units/Circles.jsx
@@ -1,7 +1,21 @@
+import {motion} from 'framer-motion'
 function Circles({title, detail}){
-    return <div className="flex flex-wrap bg-gradient-to-r from-zinc-100 to-zinc-300 aspect-square sm:w-52  lg:w-72 rounded-full p-2 m-8 items-center justify-center shadow-2xl hover:-scale-x-100 delay-500 duration-1000" >
+    return <motion.div 
+    className="flex flex-wrap bg-gradient-to-r from-zinc-100 to-zinc-300 
+    aspect-square sm:w-52  lg:w-72 rounded-full p-2 m-8 items-center 
+    justify-center shadow-2xl " 
+    whileInView={{
+        scaleX:1
+    }}
+    transition={{
+        duration:0.65
+    }}
+    initial={{
+        scaleX:-1
+    }}
+    >
 
-        <span className="text-center text-xs hover:-scale-x-100 delay-500 duration-1000">
+        <span className="text-center text-xs ">
             <h1 className="  text-xl">
                 {title}
             </h1>
@@ -9,5 +23,5 @@ function Circles({title, detail}){
             {detail}
         </span>
     
-    </div>
+    </motion.div>
 }export default Circles;
