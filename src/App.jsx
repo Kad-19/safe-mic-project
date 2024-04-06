7c1e6009475299be9d737fd144fddf59ed890e7f
Senait <mengeshasenait695gmail.com>
2024-04-07 03:23:53 +0900

    contact info-1st commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,13 @@ import Reg from "./component/LoginPage/Reg";
 import Navbar from "./component/Navbar/Navbar";
 import Complaint from './component/Complaint/Complaint'
 import AllRouting from "./component/AllRouting/AllRouting";
+import CounseInfo from "./component/Counselor info/CounseInfo";
 const App = () => {
   return (
   <div> 
     <Navbar/>
     <AllRouting/>
-     
+    <CounseInfo/>
   </div>
   
 
