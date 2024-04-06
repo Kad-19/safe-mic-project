5bc39d4183ddd31417bab35ab64d9ab015738ea3
Senait <mengeshasenait695gmail.com>
2024-04-07 05:44:00 +0900

    space

--- src/component/LoginPage/LoginPage.jsx
+++ src/component/LoginPage/LoginPage.jsx
@@ -21,7 +21,7 @@ const LoginPage = () => {
         <lable htmlFor=""> Password</lable>
           <input type="password" className=" mt-1 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
          focus:outline-none focus:border-purple-500 focus:ring-1 focus:ring-purple-500
-      disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none"  required />
+      disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none" required />
       <RiLockPasswordLine className='absolute top-4 right-4'/>
         </div>
 
