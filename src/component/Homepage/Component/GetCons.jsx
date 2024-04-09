b4840c2c391ead2a079364bfd651467dbdb9b824
Kad-19 <kidusasebe1919@gmail.com>
2024-04-09 13:21:06 +0300

    UI update

--- src/component/Homepage/Component/GetCons.jsx
+++ src/component/Homepage/Component/GetCons.jsx
@@ -1,14 +1,19 @@
+import { Button } from '@/components/ui/button'
 import React from 'react'
+import { NavLink } from 'react-router-dom';
 
 function GetCons() {
   return (
-    <div> 
+    <div className='flex flex-col items-center justify-center'> 
     <div className="mt-64 text-3xl font-medium text-center text-black max-md:mt-10">
       Our team is ready to help
     </div>
-    <div className="justify-center px-7 py-5 mt-16 text-xl font-light text-center border border-solid bg-neutral-700 border-neutral-700 rounded-[31px] text-neutral-50 max-md:px-5 max-md:mt-10">
+    <NavLink to="/counseling">
+    <Button className="justify-center px-7 py-5 mt-16 text-lg font-medium text-center border border-solid bg-neutral-700 border-neutral-700 rounded-[31px] text-neutral-50 max-md:px-5 max-md:mt-10 hover:text-neutral-700 hover:bg-neutral-50 transition-all duration-300 ease-in-out">
       Get Counseling
-    </div>
+    </Button>
+
+    </NavLink>
     </div>
   )
 }
