07972abef8f7744a781a2cbaf8d55c71e154e7d0
Senait <mengeshasenait695gmail.com>
2024-04-12 02:08:52 +0900

    counselor profile page

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,13 @@ import Navbar from "./component/Navbar/Navbar";
 import AllRouting from "./component/AllRouting/AllRouting";
 import ComplaintForm from "./component/FillingForm/ComplaintForm";
 import MyProfile from "./component/MyProfile/MyProfile";
+import CounselorProfile from "./component/Counselor info/CounselorProfile";
 const App = () => {
   return (
   <div className=""> 
     <Navbar/>
     <AllRouting/>
-    <MyProfile/>
+    <CounselorProfile/>
   </div>
   )
 }
