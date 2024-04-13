35623bb1e23a0904dc5880ec9894ec0271513d99
Natidev <53361365+Natidev@users.noreply.github.com>
2024-04-13 16:23:34 +0300

    Added disabled dates to the calendar

--- src/component/Appointment/InputBlocks/Datepicker.jsx
+++ src/component/Appointment/InputBlocks/Datepicker.jsx
@@ -3,6 +3,7 @@ import { Popover,
     PopoverTrigger} from "@radix-ui/react-popover";
 
 import { Calendar } from "@/components/ui/calendar";
+import { Card } from "@/components/ui/card";
 import { useState,useEffect } from "react";
 import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
@@ -12,6 +13,26 @@ function Datepicker(){
     const [date,setDate]=useState(new Date())
     const [dsply, setDsply]= useState("Set the date") 
     const {dateSetter}=useContext(AppointmentContext);
+    let a=new Date(2024,3,4);
+    let b=new Date(2024,5,17)
+    const [disabledDates,setDisabledDates]=useState([])
+    useEffect(()=>{
+        let c=[]
+        if(a.getDay!==6){
+            let ad=a.getDate()+6-a.getDay()
+            a.setDate(ad)
+        }
+        let sum=a.getDate()
+        let i=0
+        while(i<11){
+            c.push(new Date(2024,3,sum))
+            c.push(new Date(2024,3,sum+1))
+            sum=sum+7
+            i++;
+        }
+        c.push({from: new Date(2024,5,17), to:new Date(2024,10,1)})
+        setDisabledDates(c)
+    },[])
     return <Popover>
         <PopoverTrigger asChild>
             <Button
@@ -21,11 +42,11 @@ function Datepicker(){
             )}
         >{dsply}</Button>
         </PopoverTrigger>
-        <PopoverContent>
+        <PopoverContent  className={cn('bg-background')}>
+
         <Calendar 
         mode="single"
         selected={date}
-        className={cn(' bg-background')}
         onSelect={date=>{
             setDate(date)
             setDsply(date.toLocaleDateString("en-US",
@@ -33,10 +54,10 @@ function Datepicker(){
             month:'short',
             day:'numeric'
         }))
-        
         setDate(date)
         dateSetter(date)
         }}
+        disabled={disabledDates}
         initialFocus
         />
         </PopoverContent>
