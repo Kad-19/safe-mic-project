0d753071abe780298c2c6d9a66f6fc1208cd7926
Kad-19 <kidusasebe1919@gmail.com>
2024-04-14 22:41:25 +0300

    update

--- src/component/CounselorPage/Appo/Compo/Apoo.jsx
+++ src/component/CounselorPage/Appo/Compo/Apoo.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 import Aptime from './Aptime'
 function Apoo() {
   return (
-    <div className="flex flex-col bg-white">
+    <div className="flex flex-col">
     <div className="flex flex-col items-start pl-12 mt-1.5 w-full">
-      <div className="mt-28 ml-20 text-4xl font-semibold text-black max-md:mt-10 max-md:ml-2.5">
+      <div className="mt-28 ml-20 text-4xl font-semibold max-md:mt-10 max-md:ml-2.5">
       Your Appointments
       </div>
       <div className="flex flex-col justify-center mt-5 ml-16 md:w-3/4 text-xs leading-4 max-md:ml-2.5">
@@ -13,9 +13,6 @@ function Apoo() {
 
       </div>
       </div>
-    </div>
-    <div className="justify-center items-center px-16 pt-28 pb-28 mt-64 w-full text-xl font-light text-center text-black whitespace-nowrap bg-zinc-300 max-md:px-5 max-md:mt-10 max-md:max-w-full">
-      Footer
     </div>
   </div>
   )
--- src/component/CounselorPage/LandingPage/Comp/Landing.jsx
+++ src/component/CounselorPage/LandingPage/Comp/Landing.jsx
@@ -1,43 +1,44 @@
 import * as React from "react";
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import { BrowserRouter as Route } from 'react-router-dom'
 import Tablecon from "./Tablecon";
+import { Button } from "@/components/ui/button";
 function Landing() {
   return (
-    <div className="flex flex-col pr-1.5 bg-white">
+    <div className="flex flex-col pr-1.5">
             <div className="flex flex-col items-start mt-56 ml-32 max-w-full font-semibold w-[600px] max-md:mt-10">
         <div className="self-stretch text-5xl font-bold bg-clip-text max-md:max-w-full max-md:text-4xl">
           Welcome Back
         </div>
-        <div className="mt-64 ml-2.5 text-4xl text-black max-md:mt-10">
+        <div className="mt-64 ml-2.5 text-4xl max-md:mt-10">
           Today’s appointments
         </div>
-        <div className="flex flex-col mt-16 ml-2.5 max-w-full text-xs leading-4 text-black bg-white rounded border border-solid border-zinc-400 w-[496px] max-md:mt-10">
+        <div className="flex flex-col mt-16 ml-2.5 max-w-full text-xs leading-4 rounded w-[496px] max-md:mt-10">
          <Tablecon/>
         </div>
-        <div className="justify-center px-7 py-5 mt-14 ml-16 text-2xl bg-zinc-300 rounded-[40px] text-neutral-700 max-md:px-5 max-md:mt-10 max-md:ml-2.5">
+        <NavLink to="/counselor/all-appointments" className="p-4 py-8">
+        <Button className="">
           All appointments
-        </div>
-        <div className="mt-40 ml-2.5 text-3xl font-medium leading-10 text-black max-md:mt-10">
-          Who have messaged you
+        </Button>
+
+        </NavLink>
+        <div className="mt-40 ml-2.5 text-3xl font-medium leading-10 max-md:mt-10">
+          See who have messaged you
         </div>
         
-        <div className="flex gap-5 mt-14 ml-2.5 text-2xl max-md:flex-wrap max-md:mt-10 max-md:max-w-full">
+        <div className="flex gap-4 p-8">
 
-        <Link to='/login'>
-          <div className="grow justify-center px-8 py-6 text-white bg-neutral-600 rounded-[40px] w-fit max-md:px-5">
+        <Link to='/oneToOneChat'>
+          <Button className="">
             Go to Discussion
-          </div>
+          </Button>
           </Link>
-          <Link to='/login2'>
-          <div className="grow justify-center px-8 py-6 bg-zinc-300 rounded-[40px] text-neutral-700 w-fit max-md:pl-5">
+          <Link to='/oneToOneChat'>
+          <Button className="">
             Chat with a student
-          </div>
+          </Button>
           </Link>
         </div>
-      </div>
-      <div className="justify-center items-center px-16 pt-28 pb-28 mt-36 text-xl font-light text-center text-black whitespace-nowrap bg-zinc-300 max-md:px-5 max-md:mt-10 max-md:max-w-full">
-        Footer
       </div>
     </div>
   );
