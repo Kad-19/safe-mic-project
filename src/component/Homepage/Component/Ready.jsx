ea91b6d22cff5e65f0a547293b200b171b31cb5b
Natidev <53361365+Natidev@users.noreply.github.com>
2024-04-14 12:58:58 +0300

    adjustments to the home page

--- src/component/Homepage/Component/Ready.jsx
+++ src/component/Homepage/Component/Ready.jsx
@@ -2,6 +2,7 @@ import { Button } from '@/components/ui/button'
 import React from 'react'
 import { Card } from '@/components/ui/card'
 import { cn } from '@/lib/utils'
+import { motion } from 'framer-motion'
 // import logo from '../assets/Images/logo.png'
 function Ready() {
   return (
@@ -16,8 +17,23 @@ function Ready() {
             className=""
           />
         </Card>
-        <Card className={cn('mx-4 my-2 px-8 py-12 shadow-lg border max-sm:mx-0')}>
-              <b>About, Mental health </b>
+        <Card className={cn('mx-4 my-2 px-8 py-12 font-san shadow-lg border max-sm:mx-0')}>
+              <div className='my-4'> <b>About, Mental health</b> </div> 
+          <motion.p
+          className=' font-sans'
+          initial={{
+            opacity:0,
+            y:100
+          }}
+          whileInView={
+            {
+          opacity:1,
+          y:0
+            }}
+          transition={{duration:1,
+          delay:0.5}}
+          >
+
             Mental health is a level of psychological well-being or an absence of mental illness. 
             It's the psychological state of someone who is functioning at a satisfactory level of emotional 
             and behavioural adjustment.
@@ -25,8 +41,10 @@ function Ready() {
             autonomy, competence, inter-generational dependence, and
             self-actualization of one's intellectual and emotional potential, among others.
             <br />
-            <Button className="w-fit">
-              Get Resources
+          </motion.p
+          >
+            <Button className="w-fit my-12">
+                Get Resources
             </Button>
             </Card>
       </div>
